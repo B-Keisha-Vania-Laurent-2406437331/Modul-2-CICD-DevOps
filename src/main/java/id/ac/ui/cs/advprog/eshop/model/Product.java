85dc0c6ce5a62bdccbbe1d4f4fc73ad39494a152
kurrorro <keishavnia@gmail.com>
2026-02-10 17:34:42 +0700

    add validation and update dependencies

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Product.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Product.java
@@ -3,9 +3,17 @@ package id.ac.ui.cs.advprog.eshop.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+
 @Getter @Setter
 public class Product {
     private String productId;
+
+    @NotNull(message = "Product name cannot be empty")
     private String productName;
+
+    @NotNull(message = "Quantity cannot be empty")
+    @Min(value = 1, message = "Quantity must be greater than 0")
     private int productQuantity;
 }
